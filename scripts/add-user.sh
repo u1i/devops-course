53bf87ce2407111e59f07b49fd18f08aff7dc35c
Uli Hitzel <uli.hitzel@gmail.com>
2020-08-15 15:25:03 +0800

    changed

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -5,7 +5,7 @@ useradd $create_user
 mkdir /home/$create_user
 mkdir /home/$create_user/.ssh
 
-chown -R $create_user:$create_user /home/create_user
+chown -R $create_user:$create_user /home/$create_user
 cat ../keys/devops.pub >> authorized_keys
 chmod go-rwx /home/$create_user/.ssh
 chmod go-rwx /home/$create_user/.ssh/authorized_keys
