b99f90d3c321e1890975f436b2341929574e004f
Uli Hitzel <uli.hitzel@gmail.com>
2020-08-15 11:57:08 +0800

    changed

--- scripts/run-mongo.sh
+++ scripts/run-mongo.sh
@@ -1,5 +1,10 @@
+echo Cleanup...
 docker kill mymongo 2>/dev/null
 docker rm mymongo 2>/dev/null
 
+echo Starting MongoDB
 docker run --name mymongo -d mongo
+echo Hang on for a moment...
+sleep 10
+echo Starting Mongo Shell
 docker exec -it mymongo mongo
--- scripts/run-redis.sh
+++ scripts/run-redis.sh
@@ -1,5 +1,9 @@
+echo Cleanup...
 docker kill myredis 2>/dev/null
 docker rm myredis 2>/dev/null
-docker run --name myredis -d redis
-docker exec -it myredis redis-cli
 
+echo Starting Redis
+docker run --name myredis -d redis
+
+echo Starting Redis Shell
+docker exec -it myredis redis-cli
