89b575bf235668105e264d572d5c01198c1e1eb9
Uli Hitzel <uli.hitzel@gmail.com>
2020-08-17 21:09:07 +0800

    changed scripts

--- scripts/run-ansible.sh
+++ scripts/run-ansible.sh
@@ -1,2 +1,3 @@
 mkdir $HOME/ansible 2>/dev/null
-docker run -it -v $HOME/ansible:/etc/ansible u1ih/ansible
+#docker run -it -v $HOME/ansible:/etc/ansible u1ih/ansible
+docker run -it -v $HOME/ansible:/etc/ansible u1ih/devopscourse-ansible
